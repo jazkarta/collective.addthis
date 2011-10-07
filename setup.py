ef8920f7962f78beab72057c3f8799dfe5458798
JeanMichel FRANCOIS <toutpt@gmail.com>
2011-10-07 19:27:57 +0200

    fix setup.py to use README.rst and not .rst in long description

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.1.1'
 setup(name='collective.addthis',
       version=version,
       description="AddThis addon for Plone CMS",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
